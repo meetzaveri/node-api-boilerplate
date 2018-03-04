50e36d5db1eaf05080f646e6c4ccc90de1918b33
Meet Zaveri <meethpatel96@gmail.com>
2018-03-04 21:19:04 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+var db = require('./config/db');
+
+const app = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, (err, database) => {
+    if(err) return console.log(err);
+     // Make sure you add the database name and not the collection name
+    var newDB = database.db("node-api-boilerplate")
+    require('./app/routes')(app, newDB);
+
+    app.listen(port, () => {
+        console.log('We are live on port ', port);
+    });
+});
